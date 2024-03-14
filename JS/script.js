d6a052e626ac006f913ddbdd39fff5c1f9c3989a
davidefois96 <davidefois360@gmail.com>
2024-03-14 15:03:13 +0100

    [add] new code

---
+++ JS/script.js
@@ -0,0 +1,61 @@
+import  {contacts}  from './contacts.js';
+
+
+const {createApp} = Vue;
+
+
+createApp({
+
+  
+  
+  data(){
+
+
+    
+    return{
+      contacts,
+      
+
+
+
+    }
+    
+
+
+
+
+
+  },
+
+  
+  methods:{
+
+
+
+
+
+  },
+
+  
+  computed:{
+
+
+
+
+
+  },
+
+  
+  mounted(){
+
+    console.log(contacts);
+
+
+
+
+  }
+
+
+
+
+}).mount('#app');
